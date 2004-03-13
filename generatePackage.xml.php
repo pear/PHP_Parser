32ca6e78dc68c00a65a47fb60f5dea2a9206d3d9
Greg Beaver <cellog@php.net>
2004-03-13 20:55:44 +0000

    minor changes to package.xml generation to make it easier
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_Parser/trunk@153594 c90b9560-bf6c-de11-be94-00142212c4b1

--- generatePackage.xml.php
+++ generatePackage.xml.php
@@ -17,14 +17,16 @@ array('baseinstalldir' => 'PHP',
 'version' => '0.2',
 'packagedirectory' => 'C:/devel/PHP_Parser',
 'state' => 'devel',
-'filelistgenerator' => 'file',
+'filelistgenerator' => 'cvs',
 'dirroles' => array('Parser/tests' => 'test'),
+'exceptions' => array('generatePackage.xml.php' => 'test'), // so I can run it here easily
 'simpleoutput' => true,
 'notes' => 'Second development release.  Not even alpha yet.
 
 Re-organized into two parsers, Structure and Extendable.  Structure is the old parser, Extendable
-is designed to be overridden and provides access to every token.',
-'ignore' => array('generatePackage.xml.php', 'CVS/', '*TEMPLATE*', 'y.output', '*.bak')
+is designed to be overridden and provides access to every token.
+Added DocBlock parsing in from phpDocumentor 2.0 development',
+'ignore' => array('y.output')
 ));
 $e = $test->addDependency('PHP', '4.3', 'ge', 'php');
 if (PEAR::isError($e)) {
