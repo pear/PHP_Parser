e0d738f375cad54aa1eacd533c679d70c5508f39
Alan Knowles <alan_k@php.net>
2004-05-19 14:35:50 +0000

    use factory contructor in example
    git-svn-id: http://svn.php.net/repository/pear/packages/PHP_Parser/trunk@159021 c90b9560-bf6c-de11-be94-00142212c4b1

--- docs/examples/test1.php
+++ docs/examples/test1.php
@@ -3,7 +3,11 @@
 
 require_once 'PHP/Parser.php';
 
- print_r(PHP_Parser::staticParseFile(__FILE__));
+//print_r(PHP_Parser::staticParseFile(__FILE__));
+
+$p = PHP_Parser::factory();
+print_r($p->parseString(file_get_contents(__FILE__)));
+
 
 
 
